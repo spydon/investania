2187ecd771a01565b8db1b9a83f5fd7f898df78b
Teddy Chavez <teddy.chavez@skandia.se>
2023-03-09 16:20:37 +0100

    fix comments

--- lib/src/components/paycheck_component.dart
+++ lib/src/components/paycheck_component.dart
@@ -9,19 +9,19 @@ class PayCheckComponent extends SpriteComponent with HasGameReference {
   late int amount;
   final _random = Random();
   final double _size = 50;
-  final Vector2 _movementSpeed = Vector2(0, 2);
+  final Vector2 _movementSpeed = Vector2(0, 100);
 
   PayCheckComponent();
 
   @override
   void update(double dt) {
-    _move();
+    _move(dt);
     super.update(dt);
   }
 
   @override
   Future<void> onLoad() async {
-    amount = Random().nextInt(100);
+    amount = _random.nextInt(100);
     sprite = await Sprite.load('paycheck_placeholder.jpeg');
     size = Vector2.all(_size);
     position = Vector2(_random.nextDouble() * game.size.x / 2, 0);
@@ -29,9 +29,9 @@ class PayCheckComponent extends SpriteComponent with HasGameReference {
     return super.onLoad();
   }
 
-  void _move() {
+  void _move(double dt) {
     if (y < game.size.y) {
-      position += _movementSpeed;
+      position += _movementSpeed * dt;
     } else {
       removeFromParent();
     }
