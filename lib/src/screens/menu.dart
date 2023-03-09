75be39a876060f325048579df81470fde7dcacb0
Joakim Sjöstedt <joakim.sjostedt@skandia.se>
2023-03-09 17:40:30 +0100

    more desing on menu

--- lib/src/screens/menu.dart
+++ lib/src/screens/menu.dart
@@ -14,18 +14,74 @@ class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Container(
-        height: 400,
-        width: 400,
-        child: Column(
-          children: [
-            InkWell(
-              child: Text('Start game'),
-              onTap: () {
-                router.pushReplacementNamed('game');
-              },
+      color: Colors.transparent,
+      child: Center(
+        child: Container(
+          height: 400,
+          width: 400,
+          child: Column(
+            children: [
+              _menuButton(
+                router: router,
+                name: 'Start game',
+                routeName: 'game',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              _menuButton(
+                router: router,
+                name: 'Highscore',
+                routeName: 'highscore',
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              _menuButton(
+                router: router,
+                name: 'About',
+                routeName: 'about',
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _menuButton extends StatelessWidget {
+  const _menuButton({
+    super.key,
+    required this.router,
+    required this.name,
+    required this.routeName,
+  });
+
+  final String name;
+  final String routeName;
+  final RouterComponent router;
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.greenAccent),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: SizedBox(
+        height: 50,
+        width: 200,
+        child: InkWell(
+          child: Center(
+            child: Text(
+              name,
+              style: TextStyle(color: Colors.green),
             ),
-          ],
+          ),
+          onTap: () {
+            router.pushReplacementNamed(routeName);
+          },
         ),
       ),
     );
