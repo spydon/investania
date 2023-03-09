9474b4fcf4323dde77a06e4ed3ca2357996d97cb
lohnn <johannes@lohnn.se>
2023-03-09 14:16:26 +0100

    Added riverpod to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:investania/src/investania.dart';
 
 void main() async {
-  runApp(
-    const GameWidget.controlled(gameFactory: Investania.new),
-  );
+  runApp(const ProviderScope(child: _GameApp()));
+}
+
+class _GameApp extends ConsumerWidget {
+  const _GameApp();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return GameWidget.controlled(
+      gameFactory: () => Investania(ref),
+    );
+  }
 }
