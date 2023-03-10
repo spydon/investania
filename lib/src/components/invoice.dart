5fe87210adad4547122201352a3ab4763f2c26b0
Joakim Sjöstedt <joakim.sjostedt@skandia.se>
2023-03-10 09:59:25 +0100

    stuff

--- lib/src/components/invoice.dart
+++ lib/src/components/invoice.dart
@@ -2,15 +2,21 @@ import 'dart:math';
 
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
 
 class Invoice extends SpriteComponent {
+  Invoice({this.color = Colors.transparent});
+
   late int amount;
   late DateTime dueDate;
+  late Color color;
 
   @override
   Future<void> onLoad() async {
     amount = Random().nextInt(50);
     sprite = await Sprite.load('invoice_placeholder.png');
+    setColor(color);
+
     size = Vector2.all(50);
     add(RectangleHitbox());
   }
